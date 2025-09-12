962eab2d38c877034e8e9d008c1a66d3c0224a27
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-12 17:32:44 +0000

    For coding exp

---
+++ operators1.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age: "))
+if age <= 18:
+    print("She is a kid")
+elif age == 60:
+    print("she is oldiww")
+else:
+    print("she is women")
+    
---
+++ opertors.py
@@ -0,0 +1,10 @@
+a=8;
+b=5;
+age = int(input("Enter your age : "))
+if age >= 18:
+    print("She is hortty")
+elif age>=13:
+    print("she is cutiee")
+else:
+    print("You are pretty")
+    
