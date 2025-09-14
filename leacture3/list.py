4634cff4065ff6b98f985ccfc725acdc60269759
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-14 11:27:12 +0000

    Add every new files

---
+++ leacture3/list.py
@@ -0,0 +1,25 @@
+
+### LISTS
+
+marks1=78.3
+marks2=98.3
+marks3=68.3
+marks4=94.3
+marks5=87.3
+
+marks= [78.3,87.3,68.3,478.3,98.3]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[3])
+print(len(marks))
+
+student= ["karan",87.6,"delhhi"]
+print(student)
+print(student[0])
+student[0] = "arasuhi"
+print(student)
+
+mark=[1,23,56,89,90]
+print(mark[1:])
+print(mark[-5:])
---
+++ leacture3/methodlist.py
@@ -0,0 +1,24 @@
+# list =[50,96,25,4,21,56]
+# list1=["banana" , "apple" ,"grape"]
+# list.append(89)
+# print(list)
+# list.sort()
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+# list1.sort(reverse=True)
+# print(list1)
+# list.reverse()
+# print(list)
+# list1.reverse()
+# print(list1)
+
+##INSEEEEEEEEezrt
+
+list2=[2,1,3,1]
+list2.insert(1,5)
+print(list2)
+list2.remove(3)
+print(list2)
+list2.pop(2)
+print(list2)
