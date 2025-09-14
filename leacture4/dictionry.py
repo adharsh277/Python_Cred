2dabac631d6e39f61a23281c1ff1f0330401cd09
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-14 19:48:25 +0000

    doene

---
+++ leacture4/dictionry.py
@@ -0,0 +1,20 @@
+### dictionry
+
+# info = {
+#     "Key" : "value",
+#     "name" : "Apna collage",
+#     "learning" : "codinf",
+#     "age" : "35",
+#     "is_adult" : True,
+#     "marks" : 94.4,
+#     "topics" : ("dict" , "Set")
+# }
+# print(info)
+# print(type(info))
+# print(info["topics"])
+# print(info["learning"])
+# print(info["age"])
+
+null_dict ={}
+null_dict ["name"] = "apnacollage"
+print(null_dict)
--- leacture4/nested.py
+++ leacture4/nested.py
@@ -1,14 +1,23 @@
 student = {
-    "name" : "rahul kumar",
-    "subjects": {
+    "name" : "rahul kumar" "kishore" "ramesh" "tipu",
+    "subject" : {
         "phy" :97,
         "chem" :98,
         "math" :95,
     }
 }
 
+# print(student)
+# print(student["subjects"]["chem"])
+# print(student.keys())
+# print(len(list(student.keys())))
+# print(len(student))
+# print(student.values())
+# print(list(student.values()))
+
+pairs = list(student.items())
+print(pairs[1])
+
+new_dict ={"name": "neha","age":13}
+student.update(new_dict)
 print(student)
-print(student["subjects"]["chem"])
-print(student.keys())
-print(len(list(student.keys())))
-print(len(student))
