98a8a9a5cd55d09aa388d7366f4e4b7f98fe6fd5
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-14 11:26:40 +0000

    tuple.py

---
+++ leacture3/tuple.py
@@ -0,0 +1,18 @@
+# tup=(2,1,3,1)
+# print(tup[0])
+# print(tup[1])
+# tup[0] =5
+
+
+tup=(1,)
+print(tup)
+print(type(tup))
+
+
+tup=(1,2,3,4)
+print(tup[1:3])
+print(type(tup))
+
+tup1=(1,3,3,4,2,3,2)
+print(tup1.index(2))
+print(tup1.count(2))
