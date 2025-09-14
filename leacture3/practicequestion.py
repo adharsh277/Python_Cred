c124d00369b3af59d561d9d80f79578c7e7f915d
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-14 12:13:10 +0000

    it working one

---
+++ leacture3/practicequestion.py
@@ -0,0 +1,38 @@
+# movies = []
+# mov1=input("enter your 1st fav movie: ")
+# mov2=input("enter your 2st fav movie: ")
+# mov3=input("enter your 3st fav movie: ")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+
+
+## palindrome question
+
+
+# list1=[1,2,1]
+# list2=[1,3,4]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("This is a palindorme")
+# else:
+#     print("This is not a palindrome")
+
+
+### number of student in  A grade 
+
+# grade=("H","A","B","C","A","A","A","A","D")
+# print(grade.count("A"))
+
+
+list=["H","A","B","C","A","A","A","A","D"]
+list.sort()
+print(list)
+
