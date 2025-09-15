5f22c4bbd637655c643ad8e1d33337bfcbf83818
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-15 14:29:25 +0000

    evrything succes

---
+++ leacture4/practice.py
@@ -0,0 +1,45 @@
+
+##question1
+
+# my_dict ={
+#     "table":"a piece of furninture " "list of facts and figure",
+#     "cat":"a small animal",
+# }
+# word= input("enter your word:")
+
+# if word in my_dict:
+#     print(my_dict[word])
+# else:
+#     print("word not found")
+
+
+##quetsion 2
+
+# subject={"python" ,"java","cpp","python","javascript","java","python","java","cpp","c"}
+# print(len(subject))
+# print(subject)
+
+##question3
+
+# marks={}
+# x=int(input("enter your phy: "))
+# marks.update({"phy" : x})
+
+# y=int(input("enter your chem: "))
+# marks.update({"chem" : y})
+
+# a=int(input("enter your math: "))
+# marks.update({"math" : a})
+# print(marks)
+
+
+##question 4
+
+# value={9,"9.0"}
+# print(value)
+
+valus ={
+    ("float",9.0),
+    ("int",9),
+}
+print(valus)
