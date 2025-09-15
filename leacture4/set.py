e881202d413c5a650a1b4a991919a18247f8c724
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-15 08:36:40 +0000

    leacture 4

---
+++ leacture4/set.py
@@ -0,0 +1,28 @@
+# student={"adhi", "remya", "shivali", "shbu", 12,453,6787,655
+#          }
+# print(student)
+# print(type(student))
+# print(len(student))
+
+# cla= set() 
+# cla.add("adharsh")
+# cla.add("krishna")
+# print(type(cla))
+# print(cla)
+
+# cla1 = {"adhai", "rames", "babu", "ksih"}
+# cla1.remove("adhai",)
+# print(cla1)
+
+# collection= set()
+# collection.add(1)
+# collection.add(2)
+# collection.add("apnacollaege")
+# collection.add((1,2,3))
+# collection.clear()
+# print(collection)
+# print(len(collection))
+
+collection = {"hello" , "apnacplage", "world"}
+print(collection.pop())
+print(collection.pop())
