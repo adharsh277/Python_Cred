64431b013c7d243e7d4984763af27a5b3a3366b9
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-16 14:49:31 +0000

    for loop done

--- leacture5/forrloop.py
+++ leacture5/forrloop.py
@@ -1,4 +1,14 @@
-veggie =["pottao","krumok","tempopi"]
-print(veggie)
-for val in veggie:
-    print(val)
+# veggie =["pottao","krumok","tempopi"]
+# print(veggie)
+# for val in veggie:
+#     print(val)
+
+str ="apnacollaehe"
+
+for char in str:
+    if(char =='o'):
+        print("O found")
+        break
+    print(char)
+else:
+    print("NED")
