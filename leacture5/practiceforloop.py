3f857de8db52dc2782987ac22b3ce7e026cd1c27
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-17 08:29:40 +0000

    leacture 5 adding

---
+++ leacture5/practiceforloop.py
@@ -0,0 +1,20 @@
+# nums=[1,4,9,16,36,49,64,81,100,]
+# for el in nums:
+#     print(el)
+
+# searching tuuple in this loop
+
+# pot = (1,4,9,16,36,49,64,81,100,16)
+# x = int(input("enter the number: "))
+
+# idx = 0
+# for el in pot:
+#     if el == x:
+#         print("number found at index", idx)
+#         # break
+#     idx += 1
+# else:
+#     print("number not found")
+
+
+
