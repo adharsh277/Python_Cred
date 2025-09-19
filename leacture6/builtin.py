7bd7f8e09251280eb866f297ddfd0694c292b206
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-19 10:45:24 +0000

    adeedd

---
+++ leacture6/builtin.py
@@ -0,0 +1,16 @@
+#print funciton
+#builtnin functoion
+# print("adharsh")
+
+# range()
+
+## userdefined
+
+## defaukt parametr
+def cal_pro(a=9, b=9):
+    return a * b
+
+product = cal_pro()
+print(product)
+
+    
