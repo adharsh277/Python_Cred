a79a3ac46309378ce2a15c31e526ddc9502a13f0
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-20 14:30:26 +0000

    dnee leacture 6

--- leacture6/practicerrr.py
+++ leacture6/practicerrr.py
@@ -7,3 +7,14 @@ def calc_sum(n):
 sum = calc_sum(5)
 print(sum)
 
+## write evry recudsieve every element
+def print_list(items, idx):
+    if idx == len(items):   # base case: stop when index reaches list length
+        return
+    print(items[idx])       # print current element
+    print_list(items, idx+1)  # recursive call for next element
+
+fruits = ["apple", "litchi", "banana", "mango"]
+print_list(fruits, 0)   # start with index 0
+
+
