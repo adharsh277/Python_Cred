482119b78cd272f151b05e9f9b2644d9e5d09989
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-20 07:14:08 +0000

    hessus

--- leacture6/recursion.py
+++ leacture6/recursion.py
@@ -1,6 +1,17 @@
 def show(n):
-    if(n == 0):  # base cases
+    if n == 0:      # base case (stop when n = 0)
         return
-    print(n)
-    show(n-1)   # recursion call
-show(5)        #func acall
+    print(n)        # print current value
+    show(n-1)       # recursive call (call function again with smaller n)
+
+show(5)             # function call
+
+
+## factoiral
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
