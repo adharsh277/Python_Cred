92a058a4402c25f19cbbdff9c660e8e93a044a69
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-22 12:22:35 +0000

    dioene

---
+++ lecture8/classundobejct.py
@@ -0,0 +1,18 @@
+# class Student:
+#     name ="karna"
+#     name = "rahul"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+
+class Car:
+    colour = "Blue"
+    brand = "BMW"
+
+c1 = Car()
+print(c1.colour)
+print(c1.brand)
---
+++ lecture8/init.py
@@ -0,0 +1,13 @@
+class Student:
+    # name= "karna"
+    def __init__(self, name,marks):
+        self.name = name
+        self.marks= marks
+        # print(self)
+        print("creating a new student inn databse..")
+
+s1 = Student("Karna",98)
+print(s1.name,s1.marks)
+
+s2 = Student("Arjun",89)
+print(s2.name,s2.marks)
