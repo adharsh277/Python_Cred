fa04587725bc2e69d3ace881f0bacbdc17d0bb66
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-09-24 11:46:08 +0000

    djs

--- lecture9/del.py
+++ lecture9/del.py
@@ -1,8 +1,41 @@
-class Student:
-    def __init__(self,name):
-        self.name=name
+# class Student:
+#     def __init__(self,name):
+#         self.name=name
 
-s1= Student("Shardad")
-print(s1.name)
-del s1
-print(s1)
+# s1= Student("Shardad")
+# print(s1.name)
+# del s1
+# print(s1)
+
+class Account:
+    def __init__(self,acc_no,acc_pass):
+        self.acc_no=acc_no
+        self.__acc_pass=acc_pass
+
+    def reset_pass(self,new_pass):
+        print(self.__acc_pass)
+
+
+acc1 = Account("12345","abcde")
+
+print(acc1.acc_no)
+# print(acc1.acc_pass)
+print(acc1.reset_pass)
+
+
+
+
+### cllassss
+
+class Person:
+    __name__="Unknown"
+
+    def __hello(self):
+        print("hello boboboi")
+
+    def welcome(self):
+        self.__hello()
+
+p1=Person()
+
+print(p1.welcome())
